a6ae33bcab60f65305ab215ffa81e0ee85199aa9
Vinicius <vibrito@gmail.com>
2018-05-23 02:57:36 -0300

    Retirada de comentários na classe de teste

--- Wallet Stone Tests/UserServiceTest.m
+++ Wallet Stone Tests/UserServiceTest.m
@@ -40,7 +40,6 @@
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
@@ -84,9 +83,8 @@
 
 - (void)testPerformanceExample
 {
-    // This is an example of a performance test case.
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+        
     }];
 }
 
